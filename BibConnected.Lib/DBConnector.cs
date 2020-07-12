dc052363588f97463c312dfa19a0bc200cb9e6b8
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-12 15:15:37 +0200

    Create ExecuteSelect method to send instruction to server

--- BibConnected.Lib/DBConnector.cs
+++ BibConnected.Lib/DBConnector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// to use SQL and System Configuration
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
@@ -11,6 +12,23 @@ namespace BibConnected.Lib
 {
     public class DBConnector
     {
+
+        public static DataTable ExecuteSelect(string sqlInstructie) 
+        {
+            string constring = ConfigurationManager.ConnectionStrings["bibliotheek"].ToString();
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(sqlInstructie, constring);
+            try
+            {
+                da.Fill(ds);
+            }
+            catch(Exception fout)
+            {
+                string foutmelding = fout.Message;
+                return null;
+            }
+            return ds.Tables[0];
+        }
 
     }
 }
