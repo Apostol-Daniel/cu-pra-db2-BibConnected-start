5e582f5104b48f23412e082c9cc5a4ca2a04b87e
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-14 19:15:18 +0200

    Create ZoekCategorie method in Categorie

--- BibConnected.Lib/Categorie.cs
+++ BibConnected.Lib/Categorie.cs
@@ -10,6 +10,15 @@ namespace BibConnected.Lib
 {
     public class Categorie
     {
-
+        public static string ZoekCategorie(int cat_id)
+        {
+            string sql;
+            sql = "select * from categorie where cat_id = " + cat_id.ToString();
+            DataTable dtCategorie = DBConnector.ExecuteSelect(sql);
+            if (dtCategorie.Rows.Count > 0)
+                return dtCategorie.Rows[0]["categorie"].ToString();
+            else
+                return null;
+        }
     }
 }
